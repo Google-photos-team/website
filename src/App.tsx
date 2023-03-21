75ac1f178545dfd0f3f1f0c230711b3dbb036764
abd qaddora <57998399+AbdQaddora@users.noreply.github.com>
2023-03-21 17:55:31 +0200

    identify file structure and add pages and router

--- src/App.tsx
+++ src/App.tsx
@@ -3,46 +3,20 @@ import { useTranslation } from 'react-i18next';
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/globalStyle";
 import { Theme } from "./styles/theme";
-import { createBrowserRouter,RouterProvider,Link } from "react-router-dom";
+// Router
+import Router from "./router";
 
 function App() {
   const { t, i18n } = useTranslation();
 
-  const changeLanguageHandler = () =>
-     {
-        i18n.changeLanguage(i18n.language === "ar"?"en":"ar")
-     }
+  const changeLanguageHandler = () => {
+    i18n.changeLanguage(i18n.language === "ar" ? "en" : "ar")
+  }
 
-
-
-
-  const router = createBrowserRouter([
-    {
-      path:'/',
-      element:(
-        <div className="App">
-          <p>{t('title', { name: 'John' })}</p>
-          <button onClick={() => changeLanguageHandler()}>{i18n.language === "ar"?"en":"ar"}</button>
-          <Link to="/sample">{t('simpleLink')}</Link>
-        </div>
-      )
-    },
-    {
-      path:"sample",
-      element:(
-        <div>
-          <p>{t('description.part1')}</p>
-          <button onClick={() => changeLanguageHandler()}>{i18n.language === "ar"?"en":"ar"}</button>
-          <Link to="/">{t('homeLink')}</Link>
-        </div>
-      )
-    }
-  ])
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyle />
-
-      <RouterProvider router={router} />
+      <Router />
     </ThemeProvider>
   );
 }
