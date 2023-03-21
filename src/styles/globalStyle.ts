6474e662c55ef6879e1e5355b61466e2c849402b
notShadowM <maanabahin2001@gmail.com>
2023-03-21 16:17:32 +0200

    project setup

---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  
+  * {
+    font-family: 'Montserrat', sans-serif;
+    padding: 0;
+    margin: 0;
+    font-weight: 500;
+    box-sizing: border-box;
+  }
+`
