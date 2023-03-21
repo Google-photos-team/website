034e66c1c4e2ea47b14895e0f697aed8b4c6b505
abd qaddora <57998399+AbdQaddora@users.noreply.github.com>
2023-03-21 19:20:08 +0200

    create button component and add inter font

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Button from '../../components/Button'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <div>
+                <Button margin='16px 8px' size='large'>CLICK ME</Button>
+                <Button margin='16px 8px' color='secondary' size='large'>CLICK ME</Button>
+            </div>
+            <div>
+                <Button margin='16px 8px' size='medium'>CLICK ME</Button>
+                <Button margin='16px 8px' color='secondary' size='medium'>CLICK ME</Button>
+            </div>
+            <div>
+                <Button margin='16px 8px' size='small'>CLICK ME</Button>
+                <Button margin='16px 8px' color='secondary' size='small'>CLICK ME</Button>
+            </div>
+            <Button fullWidth>CLICK ME</Button>
+        </div>
     )
 }
 
