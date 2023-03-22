26ee820895cab87b04dcdaec1b138b0846132027
mo7mad4 <mohammedreda814@gmail.com>
2023-03-23 00:48:18 +0200

    Adding code react and file css  pure and make file styles for styled-components

---
+++ src/styles/ImagePage/styles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Item = styled.div`
+    display: flex;
+    padding: 0 4rem;
+`;
+
+export const ItemImage = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
