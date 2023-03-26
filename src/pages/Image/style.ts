f30c8ed0f069c3b555dc097110f2cf920ece7c18
mo7mad4 <mohammedreda814@gmail.com>
2023-03-26 19:02:01 +0300

    Update style menu icon

--- src/pages/Image/style.ts
+++ src/pages/Image/style.ts
@@ -1,20 +1,18 @@
 import styled from "styled-components";
 
 const Styles = styled.div`
-
-.item {
+  .item {
     display: flex;
     padding: 0 2rem;
   }
-  
+
   .itemImage {
     flex: 1;
     display: flex;
     justify-content: center;
     align-items: center;
-  
   }
-  
+
   .itemImage img {
     width: 100%;
     height: 100%;
@@ -22,7 +20,7 @@ const Styles = styled.div`
     padding: 5px;
     margin-top: 100px;
   }
-  
+
   .item-content {
     flex: 1;
     display: flex;
@@ -32,70 +30,76 @@ const Styles = styled.div`
     margin: 5rem;
     position: relative;
   }
+
+  .menu{
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    margin-top:2px;
   
+  }
+
+
+
   @media screen and (max-width: 1050px) {
-    .item{
+    .item {
       flex-direction: column;
     }
-    .item-content{
+    .item-content {
       margin: 0 0 3rem;
     }
-    .item-image{
+    .item-image {
       border-right: unset;
     }
-    .item-content-detail p{
+    .item-content-detail p {
       width: 100%;
     }
-   
   }
-  
+
   @media screen and (max-width: 650px) {
-    .item-content h1{
+    .item-content h1 {
       font-size: 28px;
-    line-height: 42px;
+      line-height: 42px;
     }
-    .item-content p{
+    .item-content p {
       font-size: 16px;
       line-height: 24px;
     }
-    .item-content__people{
+    .item-content__people {
       flex-direction: column;
     }
-    .item-content__people p{
+    .item-content__people p {
       margin: 0;
     }
 
-  
+    
+    
   }
-  @media screen and (max-width: 490px){
-    .item-content h1{
+  @media screen and (max-width: 490px) {
+    .item-content h1 {
       font-size: 28px;
-    line-height: 42px;
+      line-height: 42px;
     }
-    .item-content p{
+    .item-content p {
       font-size: 14px;
       line-height: 26px;
     }
-    .item{
-      padding-inline:1rem !important ;
+    .item {
+      padding-inline: 1rem !important ;
       padding-top: 1rem !important;
     }
-    .item-image img{
+    .item-image img {
       padding: 0;
       border-radius: 20px;
       margin-bottom: 25px;
     }
-    .item-content-action{
+    .item-content-action {
       top: 15%;
     }
-    .item-content-action p{
+    .item-content-action p {
       margin-inline: 5px;
-      
     }
-   
   }
-
-`
+`;
 
 export default Styles;
-
