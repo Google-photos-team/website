3202a59a1bb6a3115794c578db111ac776067fa5
mo7mad4 <mohammedreda814@gmail.com>
2023-03-29 01:13:33 +0300

    Adding Span from Typography

--- src/pages/Image/components/Hashtags/index.tsx
+++ src/pages/Image/components/Hashtags/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Tag from "../../../../components/Tag";
+import { Span } from "../../../../components/Typography";
 import HashtagsStyle from "./style";
 
 const Hashtags = () => {
