3ec2d65704290f4574aa579c93b5121c329af979
abd qaddora <57998399+AbdQaddora@users.noreply.github.com>
2023-05-16 00:11:11 +0300

    local

--- src/pages/Image/components/Menu/index.tsx
+++ src/pages/Image/components/Menu/index.tsx
@@ -1,38 +1,51 @@
 import React, { useState } from "react";
-import Modal from "../../../../components/Modal";
+import ModalV2 from "../../../../components/ModalV2";
 import DeleteImage from "../../../../modals/DeleteImageModal";
-import MoveImage from '../../../../modals/MoveImageModal'
 import MenuStyle from "./style";
+import MoveImageModal from "../../../../modals/MoveImageModal";
 
-const Menu = () => {
-    const [moveModal, setMoveModal] = useState(false);
-    const [deleteModal, setdeleteModal] = useState(false);
+interface props {
+    typeOne: string,
+    typeTwo: string,
+    className: string,
+    image_id: string,
+    folder_id: string,
+    closeMenu: () => void,
+    setFolderId: (folder_id: string) => void,
+}
+
+const Menu = ({ className, typeOne, typeTwo, closeMenu, setFolderId, folder_id, image_id }: props) => {
+    const [isMoveModalOpen, setIsMoveModalOpen] = useState(false);
+    const [deleteModal, setDeleteModal] = useState(false);
     return (
-        <MenuStyle>
-            <div className="menu">
+        <>
+            {isMoveModalOpen && <ModalV2 close={() => { setIsMoveModalOpen(false); }}>
+                <MoveImageModal
+                    setFolderId={setFolderId}
+                    close={() => setIsMoveModalOpen(false)}
+                    {...{ folder_id, image_id }} />
+            </ModalV2>}
 
-                <ul className="menuTypes">
-                    <div onClick={() => setMoveModal(true)}>
-                        <li className="move">Move</li>
-                    </div>
-                    <div onClick={() => setdeleteModal(true)}>
-                        <li className="delete">Delete</li>
-                    </div>
-                </ul>
-            </div>
-            <Modal
-                close={() => setMoveModal(false)}
-                isOpen={moveModal}
-                Content={MoveImage}
+            {deleteModal && <ModalV2 close={() => setDeleteModal(false)}>
+                <DeleteImage
+                    close={() => setDeleteModal(false)}
+                    {...{ folder_id, image_id }}
+                />
+            </ModalV2>}
 
-            />
-            <Modal
-                close={() => setdeleteModal(false)}
-                isOpen={deleteModal}
-                Content={DeleteImage}
-
-            />
-        </MenuStyle>
+            <MenuStyle onClick={(e) => e.stopPropagation()}>
+                <div className={className}>
+                    <ul className="menuTypes">
+                        <div onClick={() => setIsMoveModalOpen(true)}>
+                            <li className="move">{typeOne}</li>
+                        </div>
+                        <div onClick={() => setDeleteModal(true)}>
+                            <li className="delete">{typeTwo}</li>
+                        </div>
+                    </ul>
+                </div>
+            </MenuStyle>
+        </>
 
     );
 }
